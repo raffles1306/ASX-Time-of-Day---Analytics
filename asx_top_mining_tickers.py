246e021000785eaf50528e8da4880b92185d77dd
raffles1306 <brotherwoodkane@gmail.com>
2025-11-17 15:45:52 +0800

    Add files via upload

---
+++ asx_top_mining_tickers.py
@@ -0,0 +1,8 @@
+#  List of active, liquid ASX mining/resource stocks (2025)
+TOP_ASX_MINING = [
+    'BHP.AX', 'RIO.AX', 'FMG.AX', 'MIN.AX', 'IGO.AX', 
+    'PLS.AX', 'LTR.AX', 'S32.AX', 'NST.AX', 'EVN.AX',
+    'WHC.AX', 'SYR.AX', 'PDN.AX', 'BOE.AX', 'CXO.AX',
+    'RMS.AX', 'RRL.AX', 'SBM.AX', 'GOR.AX', 'RSG.AX',
+    'AZJ.AX', 'LYC.AX', 'DRE.AX', 'SYA.AX', 'LKE.AX'
+]
